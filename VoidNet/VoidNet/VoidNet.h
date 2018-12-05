0153c99adb8df0a7c55c621d14b1c1c199b9d41b
kangy <kangying@xin.com>
2018-12-05 16:08:43 +0800

    init workspace

---
+++ VoidNet/VoidNet/VoidNet.h
@@ -0,0 +1,19 @@
+//
+//  VoidNet.h
+//  VoidNet
+//
+//  Created by ciefky on 2018/12/5.
+//  Copyright © 2018 ciefky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VoidNet.
+FOUNDATION_EXPORT double VoidNetVersionNumber;
+
+//! Project version string for VoidNet.
+FOUNDATION_EXPORT const unsigned char VoidNetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VoidNet/PublicHeader.h>
+
+
